b7bc5080a65ab4350df703bf1e3e502adfd57e2e
Umair Saleem <saleemakhtar35302@gmail.com>
2023-12-10 13:29:48 +0500

    update ConfigModule setup

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,10 +1,11 @@
 import Joi from 'joi';
+import * as path from 'path';
 import { ConfigModuleOptions } from '@nestjs/config';
 import { ENVIRONMENT } from '../constants';
 
 export const Config: ConfigModuleOptions = {
   isGlobal: true,
-  envFilePath: `.env.${process.env.NODE_ENV || ENVIRONMENT.DEV}`,
+  envFilePath: path.join(__dirname, `../../.env.${process.env.NODE_ENV}`),
   validationSchema: Joi.object({
     PORT: Joi.number().default(5000),
     NODE_ENV: Joi.string()
